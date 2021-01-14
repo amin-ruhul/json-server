ec5ce8af81e4338af628aa49d975a665fda26240
amin_ruhul <ruhulinfo50@gmail.com>
2021-01-14 22:20:15 +0600

    render data

--- js/details.js
+++ js/details.js
@@ -1,1 +1,20 @@
 // javascript for details.html
+
+const container = document.querySelector(".details");
+
+const id = new URLSearchParams(window.location.search).get("id");
+
+const renderDetails = async () => {
+  const response = await fetch("http://localhost:3000/posts/" + id);
+  const post = await response.json();
+
+  const template = `
+        <h2>${post.title}</h2>
+        <p><small>${post.likes} likes</small></p>
+        <p>${post.body}</p>
+  `;
+
+  container.innerHTML = template;
+};
+
+window.addEventListener("DOMContentLoaded", () => renderDetails());
--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ const renderData = async () => {
             <h2>${post.title}</h2>
             <p><small>${post.likes} likes</small></p>
             <p>${post.body.slice(0, 200)}</p>
-            <a href="/details.html">read more...</a>   
+            <a href="/details.html?id=${post.id}">read more...</a>   
       </div>
       
       `;
